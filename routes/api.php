bdbb9e0d6dde81f4dba87740c7495282b4821499
= <=>
2019-07-21 03:18:24 +0700

    .

--- routes/api.php
+++ routes/api.php
@@ -18,29 +18,29 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['prefix' => 'products'], function () {
+// Route::group(['prefix' => 'products'], function () {
 
-    Route::get('/', 'ApiProductController@index');
-    Route::get('/{id}', 'ApiProductController@show');
-    Route::post('', 'ApiProductController@store');
-    Route::put('/{id}', 'ApiProductController@update');
-    Route::delete('/{id}', 'ApiProductController@delete');
-});
+//     Route::get('/', 'ApiProductController@index');
+//     Route::get('/{id}', 'ApiProductController@show');
+//     Route::post('', 'ApiProductController@store');
+//     Route::put('/{id}', 'ApiProductController@update');
+//     Route::delete('/{id}', 'ApiProductController@delete');
+// });
 
-Route::group(['prefix' => 'categories'], function () {
+// Route::group(['prefix' => 'categories'], function () {
 
-    Route::get('/', 'ApiCategoryController@index');
-    Route::get('/{id}', 'ApiCategoryController@show');
-    Route::post('', 'ApiCategoryController@store');
-    Route::put('/{id}', 'ApiCategoryController@update');
-    Route::delete('/{id}', 'ApiCategoryController@delete');
-});
+//     Route::get('/', 'ApiCategoryController@index');
+//     Route::get('/{id}', 'ApiCategoryController@show');
+//     Route::post('', 'ApiCategoryController@store');
+//     Route::put('/{id}', 'ApiCategoryController@update');
+//     Route::delete('/{id}', 'ApiCategoryController@delete');
+// });
 
-Route::group(['prefix' => 'users'], function () {
+// Route::group(['prefix' => 'users'], function () {
 
-    Route::get('/', 'ApiUserController@index');
-    Route::get('/{id}', 'ApiUserController@show');
-    Route::post('', 'ApiUserController@store');
-    Route::put('/{id}', 'ApiUserController@update');
-    Route::delete('/{id}', 'ApiUserController@delete');
-});
+//     Route::get('/', 'ApiUserController@index');
+//     Route::get('/{id}', 'ApiUserController@show');
+//     Route::post('', 'ApiUserController@store');
+//     Route::put('/{id}', 'ApiUserController@update');
+//     Route::delete('/{id}', 'ApiUserController@delete');
+// });
